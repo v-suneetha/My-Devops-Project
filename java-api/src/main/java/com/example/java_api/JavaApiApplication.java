d64b69826676f40a4061431195fed736934a5f71
GGKCoder <coderggk@gmail.com>
2025-07-11 15:56:57 +0530

    Add Basic Spring boot project

---
+++ java-api/src/main/java/com/example/java_api/JavaApiApplication.java
@@ -0,0 +1,15 @@
+package com.example.java_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaApiApplication.class, args);
+		System.out.println("Hello from springboot");
+	}
+
+}
