273b463ce5553f0a2d184962a3e93c0a3f2c7844
GGKCoder <coderggk@gmail.com>
2025-07-13 19:57:58 +0530

    Added post method and alert output file

--- python-alert/alert.py
+++ python-alert/alert.py
@@ -1,5 +1,7 @@
 import requests
 try:
+    
+    requests.post('http://localhost:8080/tasks', data="stay healthy")
     response = requests.get('http://localhost:8080/tasks', timeout=5)
     if response.status_code == 200:
         print("API is working")
