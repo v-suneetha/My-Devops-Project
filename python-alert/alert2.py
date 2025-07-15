6f65aa19c97cf96c8baa7cbe66f1f1f24ef99803
GGKCoder <coderggk@gmail.com>
2025-07-15 15:37:44 +0530

    Deployed java-api & python-alert in kubernetes

--- python-alert/alert2.py
+++ python-alert/alert2.py
@@ -1,10 +1,16 @@
 import requests
 
-# In this file we included redirection of output with in this file without using > redirection in linux to avoid encoding issues.
-try:
-    requests.post('http://host.docker.internal:8080/tasks', data="stay healthy")
-    response = requests.get('http://host.docker.internal:8080/tasks', timeout=5)
 
+try:
+    # UnComment below code when you want to run it in docker workspace.
+    '''requests.post('http://host.docker.internal:8080/tasks', data="stay healthy")
+    response = requests.get('http://host.docker.internal:8080/tasks', timeout=5)'''
+    
+    # Updating url with kubernetes java-api-service to make it run inside kubernetes cluster.
+    requests.post('http://java-api-service/tasks', data="stay healthy")
+    response = requests.get('http://java-api-service/tasks', timeout=5)
+    
+    # In this file we included redirection of output with in this file without using > redirection in linux to avoid encoding issues.
     with open("python_alert_output.txt", "w", encoding="utf-8") as f:
         if response.status_code == 200:
             f.write("API is working\n")
